b1b42bd75c7bc652bb4b38bb5ae375181f7e6ef7
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-20 13:46:49 +0300

    Add and set up shutdown behavior

--- app/main.py
+++ app/main.py
@@ -11,8 +11,14 @@ async def on_startup(dp: Dispatcher):
     register_default_handlers(dp)
 
 
+async def on_shutdown(dp: Dispatcher):
+    await dp.bot.session.close()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+
 if __name__ == '__main__':
     exchange_platform_bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
     storage = MemoryStorage()
     exchange_platform_dispatcher = Dispatcher(exchange_platform_bot, storage=storage)
-    executor.start_polling(exchange_platform_dispatcher, on_startup=on_startup)
+    executor.start_polling(exchange_platform_dispatcher, on_startup=on_startup, on_shutdown=on_shutdown)
