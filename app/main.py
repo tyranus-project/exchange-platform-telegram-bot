9d38ada4f344a73b26430d05d69bb8aee8c5792e
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-20 13:52:39 +0300

    Set up configuration logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+from loguru import logger
+
 from app.config import BOT_TOKEN
 from app.handlers.default import register_default_handlers
 from app.utils.set_bot_commands import set_default_commands
@@ -9,15 +11,19 @@ from app.utils.set_bot_commands import set_default_commands
 async def on_startup(dp: Dispatcher):
     await set_default_commands(dp)
     register_default_handlers(dp)
+    logger.info("Exchange platform bot launched!")
 
 
 async def on_shutdown(dp: Dispatcher):
+    logger.info("Shutting down...")
     await dp.bot.session.close()
     await dp.storage.close()
     await dp.storage.wait_closed()
+    logger.info("Exchange platform bot finished!")
 
 
 if __name__ == '__main__':
+    logger.info("Starting Exchange platform bot...")
     exchange_platform_bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
     storage = MemoryStorage()
     exchange_platform_dispatcher = Dispatcher(exchange_platform_bot, storage=storage)
