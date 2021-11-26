e4fd1be0634b1f6cacedbb276f315970c0ca2efa
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-26 21:03:51 +0300

    Change type of states storage to RedisStorage2

--- app/config.py
+++ app/config.py
@@ -5,3 +5,7 @@ env = Env()
 env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
+
+REDIS_HOST = env.str("REDIS_HOST")
+REDIS_PASSWORD = env.str("REDIS_PASSWORD")
+REDIS_PORT = env.str("REDIS_PORT")
--- app/loader.py
+++ app/loader.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from app.config import BOT_TOKEN
+from app.config import BOT_TOKEN, REDIS_HOST, REDIS_PORT
 
 
 exchange_platform_bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
+storage = RedisStorage2(REDIS_HOST, REDIS_PORT, db=5)
 exchange_platform_dispatcher = Dispatcher(exchange_platform_bot, storage=storage)
